3d91d6ca00d3629165e4097a77bcf48ae3cc1f90
Helin Wang <49939413+WangHelin1997@users.noreply.github.com>
2024-12-12 00:01:30 -0500

    Update inference.sh

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -2,13 +2,14 @@
 
 export CUDA_VISIBLE_DEVICES=0
 
-python EAT/inference/inference.py  \
-    --source_file='EAT/inference/test.wav' \
+python EAT/inference/inference2.py  \
+    --source_path='/data/lmorove1/hwang258/Speech-Captioning-Dataset/noisy_audios2/no_vocals' \
+    --save_path="/data/lmorove1/hwang258/Speech-Captioning-Dataset/audio_tagging.txt" \
     --label_file='EAT/inference/labels.csv' \
     --model_dir='EAT' \
-    --checkpoint_dir='/hpc_stor03/sjtu_home/wenxi.chen/model_ckpt/EAT/EAT_finetuned_AS2M.pt' \
+    --checkpoint_dir='/data/lmorove1/hwang258/Speech-Captioning-Dataset/fairseq/EAT/EAT-large_epoch20_ft.pt' \
     --target_length=1024 \
-    --top_k_prediction=12 \
+    --top_k_prediction=12
 
 # For optimal performance, 1024 is recommended for 10-second audio clips. (128 for 1-second)
 # However, you should adjust the target_length parameter based on the duration and characteristics of your specific audio inputs.
